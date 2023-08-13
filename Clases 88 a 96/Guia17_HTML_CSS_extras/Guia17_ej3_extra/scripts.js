485bc4ad559c085ee673d6fc95c876dba6f2b2b7
Maxi Sternhein <maxi0062009@hotmail.com>
2023-08-12 21:34:41 -0300

    ej extras

---
+++ Clases 88 a 96/Guia17_HTML_CSS_extras/Guia17_ej3_extra/scripts.js
@@ -0,0 +1,7 @@
+function redirectToInicio() {
+    window.location.href = '../Guia17_ej3_extra.html';
+}
+
+document.querySelector(".volver").addEventListener("click", function () {
+    window.history.back();
+});
